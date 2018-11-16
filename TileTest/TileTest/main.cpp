89280077fcbce7f3a0af26b5686a45939d59829f
unknown <lean1998@hotmail.com>
2018-11-16 12:30:05 -0300

    added STP TileMap support

--- TileTest/TileTest/main.cpp
+++ TileTest/TileTest/main.cpp
@@ -1,24 +1,33 @@
-#include <SFML/Graphics.hpp>
+#include "SFML/Graphics.hpp"
+#include "STP/TMXLoader.hpp"
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
-
-	while (window.isOpen())
+	sf::RenderWindow window(sf::VideoMode(800, 600), "STP Example");
+	tmx::TileMap map("res/mapo.tmx");
+	map.ShowObjects(); // Display all the layer objects.
+	sf::View view(sf::FloatRect(200.f, 200.f, 300.f, 200.f));
+	
+										   // Start the game loop
+	while (window.isOpen()) 
 	{
+		// Process events
 		sf::Event event;
-		while (window.pollEvent(event))
+		while (window.pollEvent(event)) 
 		{
+			// Close window : exit
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
-
+		// Clear screen
+		view.move(1.f, 1.f);
 		window.clear();
-		window.draw(shape);
+		window.setView(view);
+		// Draw the map
+		window.draw(map);
+		// Update the window
 		window.display();
 	}
 
-	return 0;
+	return EXIT_SUCCESS;
 }
