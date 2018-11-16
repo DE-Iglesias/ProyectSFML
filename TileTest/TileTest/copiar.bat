2fc1332539d1715cea36d42743d779fa4ce90ea7
unknown <pehuen-testa@hotmail.com>
2018-11-16 11:14:37 -0300

    added .bat file

---
+++ TileTest/TileTest/copiar.bat
@@ -0,0 +1,3 @@
+copy /Y %1Librerias\SFML\bin\sfml-graphics%3-2.dll  %1bin\Win32\%2
+copy /Y %1Librerias\SFML\bin\sfml-system%3-2.dll  	%1bin\Win32\%2 
+copy /Y %1Librerias\SFML\bin\sfml-window%3-2.dll  	%1bin\Win32\%2
