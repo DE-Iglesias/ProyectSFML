c607d2220d505923bd3af3e0824f649785f37829
Damian Iglesias <damian.e.iglesias@gmail.com>
2018-11-23 10:44:24 -0300

    added camara scrolling

--- TileTest/TileTest/main.cpp
+++ TileTest/TileTest/main.cpp
@@ -1,28 +1,48 @@
 #include "SFML/Graphics.hpp"
 #include "STP/TMXLoader.hpp"
+#include <iostream>
+using namespace std;
+
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(800, 600), "STP Example");
+	sf::RenderWindow window(sf::VideoMode(800, 600), "SideScroll 1.0");
 	tmx::TileMap map("res/mapo.tmx");
 	map.ShowObjects(); // Display all the layer objects.
-	sf::View view(sf::FloatRect(200.f, 200.f, 300.f, 200.f));
-	
-										   // Start the game loop
+	sf::View view(sf::FloatRect(0.f, 300.f, 800.f, 600.f));
+	window.setView(view);
+	sf::Clock clock;
+	sf::Vector2f centroView;
 	while (window.isOpen()) 
 	{
-		// Process events
+		centroView = view.getCenter();
+		sf::Time elapsed = clock.restart();
 		sf::Event event;
 		while (window.pollEvent(event)) 
 		{
-			// Close window : exit
+			
 			if (event.type == sf::Event::Closed)
 				window.close();
+			
+			
 		}
+
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+		{
+			if (centroView.x < 1520)
+			view.move((200.f)*elapsed.asSeconds(), 0.f);
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+		{
+			if(centroView.x > 400)
+				view.move((-200.f)*elapsed.asSeconds(), 0.f);
+		}
+
+		
 		// Clear screen
-		view.move(1.f, 1.f);
-		window.clear();
 		window.setView(view);
+		window.clear();
 		// Draw the map
 		window.draw(map);
 		// Update the window
